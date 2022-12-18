40c20810ed2489712ef4ecce926a088857cffc18
Adnan Hussain <adnanrajput42@gmail.com>
2022-12-18 21:09:56 +0500

    feat: design post model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 # Create your models here.
 
@@ -16,3 +18,16 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    # models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
